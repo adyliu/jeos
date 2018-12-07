6787d8d964360d9d9e1de296290d7eb19296a500
Ady Liu <imxylz@gmail.com>
2018-12-07 16:56:14 +0800

    fixed serialization failed on TransactionAction

--- src/main/java/io/jafka/jeos/core/common/transaction/TransactionAuthorization.java
+++ src/main/java/io/jafka/jeos/core/common/transaction/TransactionAuthorization.java
@@ -2,9 +2,12 @@ package io.jafka.jeos.core.common.transaction;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.NonFinal;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransactionAuthorization {
 
     private String actor;
