cfa8bee42c450d88b43fd599a2dcc26cd1b8a4bc
Ady Liu <imxylz@gmail.com>
2018-12-07 17:09:02 +0800

    remove unused imports

--- src/main/java/io/jafka/jeos/core/common/transaction/TransactionAuthorization.java
+++ src/main/java/io/jafka/jeos/core/common/transaction/TransactionAuthorization.java
@@ -3,7 +3,6 @@ package io.jafka.jeos.core.common.transaction;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.NonFinal;
 
 @Data
 @AllArgsConstructor
