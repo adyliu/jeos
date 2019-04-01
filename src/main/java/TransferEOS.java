4d418190818bf65c2c4ca8e440f5aced9f6d774b
Charm <404373224@qq.com>
2019-04-01 18:35:15 +0800

    修改demo，支持自定义action发送

--- src/main/java/TransferEOS.java
+++ src/main/java/TransferEOS.java
@@ -1,3 +1,4 @@
+import java.nio.ByteBuffer;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -8,9 +9,13 @@ import java.util.List;
 import java.util.Map;
 
 import io.jafka.jeos.LocalApi;
+import io.jafka.jeos.convert.Packer;
 import io.jafka.jeos.core.common.SignArg;
+import io.jafka.jeos.core.request.chain.AbiJsonToBinRequest;
 import io.jafka.jeos.core.request.chain.transaction.PushTransactionRequest;
 import io.jafka.jeos.core.response.chain.TableRow;
+import io.jafka.jeos.util.KeyUtil;
+import io.jafka.jeos.util.Raw;
 import org.apache.log4j.BasicConfigurator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -30,13 +35,16 @@ import io.jafka.jeos.impl.EosApiServiceGenerator;
 
 public class TransferEOS {
 
-    static void transfer(EosApi client) throws Exception {
+    static void transfer() throws Exception {
         ObjectMapper mapper = EosApiServiceGenerator.getMapper();
 
-        final String from = "shijiebanggg";
+        final String from = "aqdapserver1";
 
+        EosApi eosApi = EosApiFactory.create("http://jungle2.cryptolions.io:80");
+        SignArg arg = eosApi.getSignArg(120);
+        EosApi client = eosApi;
         // ① pack transfer data
-        TransferArg transferArg = new TransferArg(from, "shijiebangmm", "0.9999 EOS", "我是中国人");
+        TransferArg transferArg = new TransferArg(from, "qiuguochao22", "0.0010 EOS", "我是中国人");
         AbiJsonToBin data = client.abiJsonToBin("eosio.token", "transfer", transferArg);
         System.out.println("bin= " + data.getBinargs());
 
@@ -54,68 +62,35 @@ public class TransferEOS {
 
         // ⑤ build the packed transaction
         PackedTransaction packedTransaction = new PackedTransaction();
-        packedTransaction.setRefBlockPrefix(block.getRefBlockPrefix());
-        packedTransaction.setRefBlockNum(block.getBlockNum());
-        // expired after 3 minutes
-        String expiration = ZonedDateTime.now(ZoneId.of("GMT")).plusMinutes(3).truncatedTo(ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        //packedTransaction.setExpiration(expiration);
-        packedTransaction.setRegion("0");
+        packedTransaction.setExpiration(arg.getHeadBlockTime().plusSeconds(arg.getExpiredSecond()));
+        packedTransaction.setRefBlockNum(arg.getLastIrreversibleBlockNum());
+        packedTransaction.setRefBlockPrefix(arg.getRefBlockPrefix());
+
         packedTransaction.setMaxNetUsageWords(0);
         packedTransaction.setMaxCpuUsageMs(0);
+        packedTransaction.setDelaySec(0);
         packedTransaction.setActions(actions);
-
-        // ⑥ unlock the creator's wallet
-        try {
-            client.unlockWallet("default", "PW5KGXiGoDXEM54YWn6yhjCmNkAwpyDemLUqRaniAwuhTArciS6j9");
-        } catch (EosApiException ex) {
-            System.err.println(ex.getMessage());
-        }
-
-        // ⑦ sign the transaction
-        SignedPackedTransaction signedPackedTransaction = client.signTransaction(packedTransaction, //
-                Arrays.asList("EOS7XP7Ks7j68Uh64HGTEeiaMsgAgKcuZbYAAf86SoPLBpxcBX5it"), //
-                "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca");
-
-        System.out.println("signedPackedTransaction=" + mapper.writeValueAsString(signedPackedTransaction));
-        System.out.println("\n--------------------------------\n");
-
-        // ⑧ push the signed transaction
-        PushedTransaction pushedTransaction = client.pushTransaction("none", signedPackedTransaction);
-        System.out.println("pushedTransaction=" + mapper.writeValueAsString(pushedTransaction));
+        String privateKey = "5JjmMFopR7Ha1ywAcFg5SxTTA8XbaUHuB1NappjKCiYwJXqBMYn";
+        String hash = sign(privateKey, arg, packedTransaction);
+        PushTransactionRequest req = new PushTransactionRequest();
+        req.setTransaction(packedTransaction);
+        req.setSignatures(Arrays.asList(hash));
+        LocalApi localApi = EosApiFactory.createLocalApi();
+        PushedTransaction pts = eosApi.pushTransaction(req);
+        System.out.println(localApi.getObjectMapper().writeValueAsString(pts));
     }
 
-    //    public static void main(String[] args) throws Exception {
-//        BasicConfigurator.configure();
-//        EosApi client = EosApiFactory.create("http://127.0.0.1:8900", "http://jungle2.cryptolions.io:80", "http://jungle2.cryptolions.io:80");
-//        ;
-//        // ------------------------------------------------------------------------
-//        transfer(client);
-//    }
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+        //BasicConfigurator.configure();
+        transfer();
+    }
 
-        // --- get the current state of blockchain
-        EosApi eosApi = EosApiFactory.create("http://jungle2.cryptolions.io:80");
-        SignArg arg = eosApi.getSignArg(120);
-        System.out.println(eosApi.getObjectMapper().writeValueAsString(arg));
+    private static String sign(String privateKey, SignArg arg, PackedTransaction t) {
 
-        // --- sign the transation of token tansfer
-//        String privateKey = "5JjmMFopR7Ha1ywAcFg5SxTTA8XbaUHuB1NappjKCiYwJXqBMYn";//replace the real private key
-//        String from = "aqdapserver1";
-//        String to = "qiuguochao22";
-//        String quantity = "0.0010 EOS";
-//        String memo = "sent by eos sdk (https://github.com/adyliu/jeos";
-//        LocalApi localApi = EosApiFactory.createLocalApi();
-//        PushTransactionRequest req = localApi.transfer(arg, privateKey, from, to, quantity, memo);
-//        System.out.println(localApi.getObjectMapper().writeValueAsString(req));
-//
-//
-//        // --- push the signed-transaction to the blockchain
-//        PushedTransaction pts = eosApi.pushTransaction(req);
-//        System.out.println(localApi.getObjectMapper().writeValueAsString(pts));
-//
-        TableRow result = eosApi.getTableRows("aqdapserver1","aqdapserver1","airquality",
-                "8","9", "10");
-        System.out.println("11");
+        Raw raw = Packer.packPackedTransaction(arg.getChainId(), t);
+        raw.pack(ByteBuffer.allocate(33).array());// TODO: what's this?
+        String hash = KeyUtil.signHash(privateKey, raw.bytes());
+        return hash;
     }
 
 }
