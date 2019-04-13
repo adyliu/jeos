df6c094e15e30dfc9564d72722e30b9d7bcd8863
zhujk <zhujk@outlook.com>
2019-04-13 23:28:23 +0800

    transfer:support other tokens
    Signed-off-by: zhujk <zhujk@outlook.com>

--- src/main/java/io/jafka/jeos/LocalApi.java
+++ src/main/java/io/jafka/jeos/LocalApi.java
@@ -1,14 +1,13 @@
 package io.jafka.jeos;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.jafka.jeos.core.common.SignArg;
 import io.jafka.jeos.core.request.chain.transaction.PushTransactionRequest;
 import io.jafka.jeos.impl.EosApiServiceGenerator;
 
 /**
  * Local API with EOS without RPC
- * 
+ *
  * @author adyliu (imxylz@gmail.com)
  * @since 2018年9月6日
  */
@@ -18,8 +17,12 @@ public interface LocalApi {
 
     String toPublicKey(String privateKey);
 
-    PushTransactionRequest transfer(SignArg arg,String privateKey, String from, String to, String quantity, String memo);
-    
+    default PushTransactionRequest transfer(SignArg arg, String privateKey, String from, String to, String quantity, String memo) {
+        return transfer(arg, privateKey, "eosio.token", from, to, quantity, memo);
+    }
+
+    PushTransactionRequest transfer(SignArg arg, String privateKey, String account, String from, String to, String quantity, String memo);
+
     default ObjectMapper getObjectMapper() {
         return EosApiServiceGenerator.getMapper();
     }
