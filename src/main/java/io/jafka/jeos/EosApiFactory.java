6d155d0024c6c1724d1561e68c26e5cc0bf78c54
Ady Liu <imxylz@gmail.com>
2019-12-11 15:06:58 +0800

    add endpoint address on the javadoc

--- src/main/java/io/jafka/jeos/EosApiFactory.java
+++ src/main/java/io/jafka/jeos/EosApiFactory.java
@@ -4,7 +4,13 @@ import io.jafka.jeos.impl.EosApiRestClientImpl;
 import io.jafka.jeos.impl.LocalApiImpl;
 
 public abstract class EosApiFactory {
-
+    /**
+     * create api from endpoint
+     * <p>server list: https://www.eosdocs.io/resources/apiendpoints/</p>
+     * @param baseUrl the endpoint address
+     * @return api client
+     * @see https://www.eosdocs.io/resources/apiendpoints/
+     */
     public static EosApi create(String baseUrl) {
         return new EosApiRestClientImpl(baseUrl);
     }
